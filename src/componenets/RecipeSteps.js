5c6f747426089b5bef4bda3e9bf8e836625613b6
rahulrwt <rahulrawat.rrc@gmail.com>
2021-03-13 13:28:44 +0530

    working on RecipeSteps

--- src/componenets/RecipeSteps.js
+++ src/componenets/RecipeSteps.js
@@ -1,14 +1,17 @@
-import { Typography } from '@material-ui/core';
+import { Paper,  Typography } from '@material-ui/core';
 import React from 'react';
-
+ 
 const RecipeSteps=( {step})=>{
  
 
       
   return (
       <>
+      <div style={{margin:20}}>
+      <Paper variant="outlined" style={{margin:30}} />
         <h4>Step {step.number}  </h4>
         <Typography>{step.step}</Typography>
+        </div>
          </>
   )
 };
