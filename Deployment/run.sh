0d848b04a0bfdfb618881e952b722dd2d82df26e
Rahul Rawat <Rahul@Rahuls-MacBook-Air.local>
2023-10-31 21:44:26 +0530

    temp change

--- Deployment/run.sh
+++ Deployment/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+aws --version
 aws s3 cp s3://cooking-corner-codepipeline/credentials/awsCred.txt . 
+sleep 10
 echo "AWS keys downloaded"
+aws sts get-caller-identity
 ls
-echo "PWD"
-pwd
 source awsCred.txt
 export AWS_ACCESS_KEY_ID=$aws_access_key_id
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
