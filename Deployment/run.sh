f1d9af3420be4e78ae95e3bcd02e32440a384522
Rahul Rawat <Rahul@Rahuls-MacBook-Air.local>
2023-10-31 20:59:31 +0530

    edit workflow

--- Deployment/run.sh
+++ Deployment/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-aws s3 cp s3://cooking-corner-codepipeline/credentials/awsCred.txt .
+aws s3 cp s3://cooking-corner-codepipeline/credentials/awsCred.txt . 
+echo "AWS keys downloaded"
+
 source awsCred.txt
 export AWS_ACCESS_KEY_ID=$aws_access_key_id
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
