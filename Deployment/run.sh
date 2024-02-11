538e6d72abda042a199258077fdbe10a4eacaeee
rahulrwt <rahulrawat.rrc@gmail.com>
2024-02-11 14:42:01 +0530

    Add Deployment Script

--- Deployment/run.sh
+++ Deployment/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 aws --version
-aws s3 cp s3://cooking-corner-codepipeline/credentials/awsCred.txt . 
-sleep 10
-echo "AWS keys downloaded"
-aws sts get-caller-identity
+aws s3 ls
 ls
-source awsCred.txt
-export AWS_ACCESS_KEY_ID=$aws_access_key_id
-export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
-echo "AWS keys has been set successfullyy"
